8f6e571edba3642059f0561ae429f6566d9e4419
jkseppal <jani.k.seppala@gmail.com>
2020-09-01 13:39:48 +0300

    6.14 tehty, toimii

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -46,14 +46,10 @@ const anecdoteReducer = (state = [], action) => {
   }
 }
 
-export const createAnecdote = (content) => {
+export const createAnecdote = (data) => {
   return {
     type: 'NEW_ANECDOTE',
-    data: {
-      content,
-      id: getId(),
-      votes: 0
-    }
+    data,
   }
 }
 
--- redux-anecdotes/src/services/anecdotes.js
+++ redux-anecdotes/src/services/anecdotes.js
@@ -8,7 +8,8 @@ const getAll = async () => {
 }
 
 const createNew = async (content) => {
-  const response = await axios.post(baseUrl, content)
+  const object = { content, votes:0 }
+  const response = await axios.post(baseUrl, object)
   return response.data
 }
 
