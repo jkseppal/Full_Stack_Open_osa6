85d74774475fd0059c286353c9f4ee9d9029b687
jkseppal <jani.k.seppala@gmail.com>
2020-09-02 12:23:21 +0300

    6.19 tehty

--- redux-anecdotes/src/components/Notification.js
+++ redux-anecdotes/src/components/Notification.js
@@ -1,4 +1,4 @@
-import React from 'react'
+/*import React from 'react'
 import { useSelector } from 'react-redux'
 //import notificationReducer from '../reducers/notificationReducer'
 
@@ -20,4 +20,34 @@ const Notification = () => {
   )
 }
 
-export default Notification
+export default Notification*/
+
+import React from 'react'
+import { connect } from 'react-redux'
+
+const Notification = (props) => {
+  const notification = props.notification
+  const style = {
+    border: 'solid',
+    padding: 10,
+    borderWidth: 1
+  }
+
+  if (notification === '') {
+    return null
+  }
+  return (
+    <div style={style}>
+      {notification}
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notifications
+  }
+} 
+
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+export default ConnectedNotification
